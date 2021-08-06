35d715ea11f56d49f2a2c668437ea2f86d267ad6
小纯洁 <smallpurebywu@126.com>
2021-08-06 18:34:40 +0800

    mod index

--- webhook.php
+++ webhook.php
@@ -10,15 +10,12 @@ $path = "/home/wwwroot/php7.4/vona/hd.vona.xin";
 
 // Headers deliveried from GitHub
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-shell_exec("echo {$signature} >> {$path}/test.txt");
 
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
-  shell_exec("echo {$hash} >> {$path}/test.txt");
   if (strcmp($signature, $hash) == 0) {
     $exec = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
     echo $exec;
-    shell_exec("echo {$exec} >> {$path}/test.txt");
     exit();
   }
 }
