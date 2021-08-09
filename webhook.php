fdd863a5a20099c4a2d9f427c77a53fc204776d5
小纯洁 <smallpurebywu@126.com>
2021-08-09 10:49:15 +0800

    mod webhook

--- webhook.php
+++ webhook.php
@@ -14,8 +14,10 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
-    $exec = shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+  	$shell = "cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1";
+    $exec = shell_exec($shell);
     echo $exec;
+    shell_exec("cd {$path} && echo {$shell} >> test.txt 2>&1");
     shell_exec("cd {$path} && echo {$exec} >> test.txt 2>&1");
     exit();
   }
